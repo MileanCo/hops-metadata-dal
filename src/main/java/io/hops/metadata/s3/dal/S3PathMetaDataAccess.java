a9a71300ab1a3f02ad904c1657d65fab83440bb6
Joel Stenkvist <joelm.sten@gmail.com>
2019-03-16 12:49:07 -0400

    added bucket param to s3guard methods

--- src/main/java/io/hops/metadata/s3/dal/S3PathMetaDataAccess.java
+++ src/main/java/io/hops/metadata/s3/dal/S3PathMetaDataAccess.java
@@ -7,21 +7,21 @@ import java.util.List;
 
 public interface S3PathMetaDataAccess<T> extends EntityDataAccess {
 
-    public T getPath(String parent, String child) throws StorageException;
+    public T getPath(String parent, String child, String bucket) throws StorageException;
     public void putPath(T path) throws StorageException;
-    public void deletePath(String parent, String child) throws StorageException;
+    public void deletePath(String parent, String child, String bucket) throws StorageException;
 
     // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_BatchWriteItem.html
     public void putPaths(List<T> paths) throws StorageException;
     public void deletePaths(List<T> paths) throws StorageException;
 
-    public boolean isDirEmpty(String parent, String child) throws StorageException;
-    public void deleteBucket(String table_name) throws StorageException ;
+    public boolean isDirEmpty(String parent, String child, String bucket) throws StorageException;
+    public void deleteBucket(String bucketName) throws StorageException ;
 
 
-    public List<T> getExpiredFiles(long modTime) throws StorageException;
+    public List<T> getExpiredFiles(long modTime, String bucket) throws StorageException;
 
-    public List<T> getPathChildren(String parent) throws StorageException;
+    public List<T> getPathChildren(String parent, String bucket) throws StorageException;
 
 
 }
